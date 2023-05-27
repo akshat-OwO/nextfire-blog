2456708599377a2faec16ac446836acd793366c2
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-05-27 18:21:14 +0530

    Google OAuth

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,44 @@
-export default function EnterPage({  }) {
+import { auth, googleAuthProvider } from '@/lib/firebase';
+import { signInWithPopup, signInAnonymously, signOut } from 'firebase/auth';
+import Image from 'next/image';
+
+export default function EnterPage({}) {
+    const user = null;
+    const username = null;
+
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ? (
+                !username ? (
+                    <UsernameForm />
+                ) : (
+                    <SignOutButton />
+                )
+            ) : (
+                <SignInButton />
+            )}
         </main>
-    )
+    );
+}
+
+// sign-in with google button
+const SignInButton = () => {
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, googleAuthProvider);
+    };
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <Image src={'/google.png'} width={30} height={30} alt="google logo" /> Sign in with Google
+        </button>
+    );
+};
+
+// sign-out
+const SignOutButton = () => {
+    return <button onClick={() => signOut(auth)}>Sign Out</button>;
+};
+
+function UsernameForm() {
+    return null;
 }
