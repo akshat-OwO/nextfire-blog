a4e1f15c160555bf3b27e41276eb0734f6099173
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-05-27 17:34:20 +0530

    dynamic navigation bar

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar({  }) {
+    const user = null;
+    const username = null;
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href='/'>
+                        <button className="btn-logo">FEED</button>
+                    </Link>
+                </li>
+
+                {/* user is signed-in and has username */}
+                {username && (
+                    <>
+                        <li className="push-left">
+                            <Link href='/admin'>
+                                <button className="btn-blue">Write Posts</button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={`/${username}`}>
+                                <Image src={user?.photoURL} alt="user profile" />
+                            </Link>
+                        </li>
+                    </>
+                )}
+
+                {/* user is not signed-in OR has not created username */}
+                {!username && (
+                    <li>
+                        <Link href='/enter'>
+                            <button className="btn-blue">Log in</button>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
